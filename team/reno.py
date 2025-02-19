091cf5e24fa54399f4bceed2b1b5fc9e5769861a
meraa <156663450+renoafrido@users.noreply.github.com>
2025-02-20 00:26:05 +0700

    Update reno.py

--- team/reno.py
+++ team/reno.py
@@ -1,18 +1,24 @@
-class bukan_Programmer:
-  def __init__(self, nama, nim, kelas, kontribusi):
-  self.nama = nama
-  self.nim = nim
-  self.kelas = kelas
-  self.kontribusi = kontribusi
+class BukanProgrammer:
+    def __init__(self, nama, nim, kelas, kontribusi):
+        self.nama = nama
+        self.nim = nim
+        self.kelas = kelas
+        self.kontribusi = kontribusi
 
-  def perkenalan(nama, nim, kelas, kontribusi):
-    print (f"Nama saya {self.nama}, kelas {self.kelas}")
-    print (f"NIM saya {self.nim}")
-    print (f"kontribusi saya:")
-    for k in self.kontribusi:
-      print(f"- {k})
-    
-kontribusi = ["Merubah tampilan menjadi lebih dinamis menggunakan jinja", 
-             "CRUD data peminjam"]
-mahasiswa = bukan_Programmer("Muh. Reno Afrido. A", 2213010493, "5I", kontribusi)
-mahasiswa.perkenalan()               
+    def perkenalan(self):
+        print(f"Nama saya {self.nama}, kelas {self.kelas}")
+        print(f"NIM saya {self.nim}")
+        print("Kontribusi saya:")
+        for k in self.kontribusi:
+            print(f"- {k}")
+
+def main():
+    kontribusi = [
+        "Merubah tampilan menjadi lebih dinamis menggunakan Jinja",
+        "CRUD data peminjam"
+    ]
+    mahasiswa = BukanProgrammer("Muh. Reno Afrido. A", 2213010493, "5I", kontribusi)
+    mahasiswa.perkenalan()
+
+if __name__ == '__main__':
+    main()
